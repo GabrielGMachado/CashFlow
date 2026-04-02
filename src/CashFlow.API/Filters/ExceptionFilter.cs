f2aa8f31747f4e1fa1b557c07bdca8651312688d
GabrielGMachado <bielgaelzermachado@gmail.com>
2026-04-01 21:37:39 -0300

    feat: add exception filter

---
+++ src/CashFlow.API/Filters/ExceptionFilter.cs
@@ -0,0 +1,30 @@
+﻿using CashFlow.Communication.Responses;
+using CashFlow.Exception.ExceptionsBase;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CashFlow.API.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is CashFlowException) { HandleProjectException(context); }
+        else { ThrowUnknownError(context); }
+
+
+    }
+
+    private void HandleProjectException(ExceptionContext context)
+    {
+
+    }
+
+    private void ThrowUnknownError(ExceptionContext context)
+    {
+        var errorMessage = new ResponseErrorJson("Unknown error");
+
+        context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Result = new ObjectResult(errorMessage);
+    }
+}
